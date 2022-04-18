4f71f7ad4adecd98e783d5bfc23dca5b3ea67ca3
Ghali Harti <hghali26@gmail.com>
2022-04-18 01:00:09 -0400

    cleaned code

--- Devoirs/INF8215_H22_TP3/my_bean_tester.py
+++ Devoirs/INF8215_H22_TP3/my_bean_tester.py
@@ -3,7 +3,7 @@ Team:
 <<<<< Ghali Harti >>>>>
 Authors:
 <<<<< Ghali Harti - 1953494 >>>>>
-<<<<< Sanmar Simon - MATRICULE #2 >>>>>
+<<<<< Sanmar Simon - 1938126 >>>>>
 """
 
 BEANS = ['SIRA','HOROZ','DERMASON','BARBUNYA','CALI','BOMBAY','SEKER']
@@ -13,14 +13,26 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import numpy as np
 
 class MyBeanTester(BeanTester):
     def __init__(self):
-        # TODO: initialiser votre modèle ici:
-        self.model = RandomForestClassifier(random_state=10, n_estimators=100, ccp_alpha=4e-5)
+        
+        self.model = make_pipeline(StandardScaler(), 
+                      PCA(n_components=15),
+                      RandomForestClassifier(random_state=70, n_estimators=300))
         pass
 
     def preprocess_y(self, y_raw):
+        """
+        Preprocess y data by removing labels and replacing class names 
+        with a number according to order in beans and to have trainable y data
+        :param y_raw: 2D array of data points.
+                each line is a different example.
+                each column is a different feature.
+                the first column is the example ID.
+        :return: a 1D of data classes without id and numerized
+        """
         classes_dic = {class_name:i for i,class_name in enumerate(BEANS)}
         y = []
         for row in y_raw:
@@ -28,6 +40,14 @@ class MyBeanTester(BeanTester):
         return y
     
     def preprocess_x(self, x_raw):
+        """
+        Preprocess x data to remove labels and to have trainable x data
+        :param x_raw: 2D array of data points.
+                each line is a different example.
+                each column is a different feature.
+                the first column is the example ID.
+        :return: a 1D of data features without id
+        """
         x = []
         for row in x_raw:
             x.append(row[2:])
@@ -67,11 +87,12 @@ class MyBeanTester(BeanTester):
         :return: a 2D list of predictions with 2 columns: ID and prediction
         """
         x = self.preprocess_x(X_data)
+        inv_classes_dic = {i:class_name for i,class_name in enumerate(BEANS)}
         y_predicted = self.model.predict(x)
-        
+        y_predicted_classes = np.vectorize(inv_classes_dic.get)(y_predicted)
         y_labeled = []
         for i in range(len(X_data)):
-            y_labeled.append([X_data[i][0], y_predicted[i]])
+            y_labeled.append([X_data[i][0], y_predicted_classes[i]])
         return y_labeled
         
         
